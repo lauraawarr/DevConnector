163611ec39192fb84a32879ad0a26280abd721f9
Laura Warr <lwarr@keyhole.co>
2018-05-06 15:09:33 -0400

    Add validation when creating profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -8,6 +8,9 @@ const Profile = require('../../models/Profile');
 // Load User Model
 const User = require('../../models/User');
 
+// Load Input Validation
+const validateProfileInput = require('../../validation/profile');
+
 // @route   GET api/profile/test
 // @desc    Tests profile route
 // @access  Public
@@ -20,6 +23,7 @@ router.get('/', passport.authenticate('jwt', {session: false}),
     (req, res) => {
         const  errors = {};
         Profile.findOne({ user: req.user.id })
+            .populate('user', ['name', 'avatar'])
             .then(profile => {
                 if (!profile) {
                     errors.noprofile = 'There is no profile for this user';
@@ -36,6 +40,13 @@ router.get('/', passport.authenticate('jwt', {session: false}),
 // @access  Private
 router.post('/', passport.authenticate('jwt', {session: false}),
     (req, res) => {
+        const { errors, isValid } = validateProfileInput(req.body);
+
+        // Check validation
+        if (!isValid) {
+            return res.status(400).json(errors)
+        }
+
         // Get fields
         const profileFields = {};
         profileFields.user = req.user.id;
@@ -69,8 +80,6 @@ router.post('/', passport.authenticate('jwt', {session: false}),
                     )
                     .then(profile => res.json(profile))
                 } else {
-                    // Create
-
                     // Check if handle exists
                     Profile.findOne({ handle: profileFields.handle })
                         .then(profile => {
