9774def3eef535028a0d9938b81157e77dcf60eb
Laura Warr <lwarr@keyhole.co>
2018-05-06 13:57:36 -0400

    Add validation to register and login routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load User model
 const User = require('../../models/User');
@@ -66,12 +67,20 @@ router.post('/login', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     // Find user by email
     User.findOne({email})
         .then(user => {
             // Check for user
             if (!user) {
-                return res.status(404).json({email: 'User not found'})
+                errors.email = 'User not found';
+                return res.status(404).json(errors)
             }
 
             // Check Password
@@ -100,7 +109,8 @@ router.post('/login', (req, res) => {
                             }
                         );
                     } else {
-                        return res.status(400).json({password: 'Password incorrect'})
+                        errors.password = 'Password incorrect'
+                        return res.status(400).json(errors)
                     }
                 })
         });
