a08aa4f07510fbaf7ca37b845a0785b22e17e757
Laura Warr <lwarr@keyhole.co>
2018-06-24 15:42:40 -0400

    Adding login to redux

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
 
-import { GET_ERRORS } from './types'
+import { GET_ERRORS, SET_CURRENT_USER } from './types'
+import setAuthToken from '../utils/setAuthToken'
+import jwt_decode from 'jwt-decode'
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
-
     axios
         .post('/api/users/register', userData)
         .then(res => history.push('/login'))
@@ -14,4 +15,36 @@ export const registerUser = (userData, history) => dispatch => {
                 payload: err.response.data
             })
         )
+}
+
+// Login - Get user token
+export const loginUser = (userData) => dispatch => {
+    axios
+        .post('/api/users/login', userData)
+        .then(res => {
+            // Save to localStorage
+            const { token } = res.data;
+            // Set token to localStorage
+            localStorage.setItem('jwtToken', token);
+            // Set token to Auth Header
+            setAuthToken(token);
+            // DEcode tokent o get user data
+            const decoded = jwt_decode(token);
+            // Set current user
+            dispatch(setCurrentUser(decoded))
+        })
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        )
+}
+
+// Set logged in user
+export const setCurrentUser = (decoded) => {
+    return {
+        type: SET_CURRENT_USER,
+        paylod: decoded,
+    }
 }
