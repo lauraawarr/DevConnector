cb71d08c30da4a3f64220628e0d160c98752d621
Laura Warr <lwarr@keyhole.co>
2018-07-22 12:09:38 -0400

    Add form to edit profile

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -18,7 +18,12 @@ class Navbar extends Component {
     const authLinks = (
         <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-                <a href="#" onClick={this.onLogoutClick.bind(this)} className="nav-link">
+              <Link className="nav-link" to="/dashboard">
+                Dashboard
+              </Link>
+            </li>
+            <li className="nav-item">
+                <a href="" onClick={this.onLogoutClick.bind(this)} className="nav-link">
                   <img
                     className="rounded-circle"
                     src={user.avatar}
