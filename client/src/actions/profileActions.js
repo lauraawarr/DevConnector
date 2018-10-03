e90b3fdfb1b48da358fe640f864cd8fe952e461a
Laura Warr <lwarr@keyhole.co>
2018-10-02 21:48:40 -0700

    Display experience and delete experience

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -59,6 +59,22 @@ export const addEducation = (eduData, history) => dispatch => {
     )
 }
 
+// DeleteExperience
+export const deleteExperience = (id) => dispatch => {
+  axios
+    .delete(`/api/profile/experience/${id}`)
+    .then(res => dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    }))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    )
+}
+
 // Delete Account & Profile
 export const deleteAccount = () => dispatch => {
   if (window.confirm('Are you sure? This can not be undone.')) {
