7690b4d7f02e80603313acbf86aa8005e970b4ed
Laura Warr <lwarr@keyhole.co>
2018-10-02 21:55:19 -0700

    Display education and delete education

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,7 @@ import { getCurrentProfile, deleteAccount } from '../../actions/profileActions'
 import Spinner from '../common/Spinner'
 import ProfileActions from './ProfileActions'
 import Experience from './Experience'
+import Education from './Education'
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -32,7 +33,7 @@ class Dashboard extends Component {
             <p className="lead text-muted">Welcome <Link to={`/profile/${profile.handle}`}>{ user.name }</Link></p>
             <ProfileActions />
             <Experience experience={profile.experience} />
-            {/* TODO: Experience and Education */}
+            <Education education={profile.education} />
             <div style={{marginBottom: '60px'}} />
             <button type="button" onClick={this.onDeleteClick.bind(this)} className="btn btn-danger">Delete My Account</button>
           </div>
