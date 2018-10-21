33c2fe5ec8c6cef7ee59988bd4d94001349f95c6
Laura Warr <lwarr@keyhole.co>
2018-10-21 11:25:57 -0700

    Start creating post feed

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -2,7 +2,9 @@ import axios from 'axios'
 
 import {
     ADD_POST,
-    GET_ERRORS
+    GET_POSTS,
+    GET_ERRORS,
+    POST_LOADING
 } from './types'
 
 // Add Post
@@ -18,3 +20,23 @@ export const addPost = (postData) => dispatch => {
             payload: err.response.data
         }))
 }
+
+// Get Posts
+export const getPosts = () => dispatch => {
+    dispatch(setPostLoading())
+    axios
+        .get('/api/posts')
+        .then(res => dispatch({
+            type: GET_POSTS,
+            payload: res.data
+        }))
+        .catch(err => dispatch({
+            type: GET_POSTS,
+            payload: null
+        }))
+}
+
+// Set loading state
+export const setPostLoading = () => ({
+    type: POST_LOADING
+})
