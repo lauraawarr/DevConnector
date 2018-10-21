a369cb8d1479f1fbdc7a4a7c2e481f8d20ef3342
Laura Warr <lwarr@keyhole.co>
2018-10-21 11:58:35 -0700

    Add ability to delete and like/unlike posts

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import {
     ADD_POST,
     GET_POSTS,
+    DELETE_POST,
     GET_ERRORS,
     POST_LOADING
 } from './types'
@@ -14,6 +15,42 @@ export const addPost = (postData) => dispatch => {
         .then(res => dispatch({
             type: ADD_POST,
             payload: res.data
+        }))
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        }))
+}
+
+// Add Like
+export const addLike = (postId) => dispatch => {
+    axios
+        .post(`/api/posts/like/${postId}`)
+        .then(res => dispatch(getPosts()))
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        }))
+}
+
+// Remove Like
+export const removeLike = (postId) => dispatch => {
+    axios
+        .post(`/api/posts/unlike/${postId}`)
+        .then(res => dispatch(getPosts()))
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        }))
+}
+
+// Delete Post
+export const deletePost = (postId) => dispatch => {
+    axios
+        .delete(`/api/posts/${postId}`)
+        .then(res => dispatch({
+            type: DELETE_POST,
+            payload: postId
         }))
         .catch(err => dispatch({
             type: GET_ERRORS,
